da23e8a64cab637f81ffc14e050d35f8f98efb58
AhsanHussainMemon <hussainahsan454@gmail.com>
2024-10-08 17:39:21 +0500

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,82 @@
+// Assignment No 11
+
+// Question No 1
+
+// var PositiveInteger = prompt("Enter a Positive Integer");
+// if (PositiveInteger > 0) {
+
+//     document.write ("Number: " + PositiveInteger + "<br>");
+//     document.write ("RoundOff: " + Math.round(PositiveInteger) + "<br>");
+//     document.write ("Floor: " + Math.floor(PositiveInteger) + "<br>");
+//     document.write ("Ceil: " + Math.ceil(PositiveInteger) + "<br>");
+// }
+
+// else {
+//     document.write ("Please Enter a Positive Integer");
+// }
+
+// Question No 2
+
+// var NegativeInteger = prompt("Enter a Negative Integer");
+// if (NegativeInteger < 0) {
+
+//     document.write ("Number: " + NegativeInteger + "<br>");
+//     document.write ("RoundOff: " + Math.round(NegativeInteger) + "<br>");
+//     document.write ("Floor: " + Math.floor(NegativeInteger) + "<br>");
+//     document.write ("Ceil: " + Math.ceil(NegativeInteger) + "<br>");
+// }
+
+// else {
+//     document.write ("Please Enter a Negative Integer");
+// }
+
+// Question No 3
+
+// var number = prompt("Enter a Number");
+// var absoluteValue = Math.abs(number);
+// document.write ("The Absolute Value Of " + number + " is " + absoluteValue);
+
+// Question No 4
+
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("Random Dice Value: " + diceValue);
+
+// Question No 5
+
+// var coinToss = Math.random(); 
+// var result; 
+
+// if (coinToss < 0.5) {
+//     result = "Heads"; 
+// } else {
+//     result = "Tails"; 
+// }
+
+// document.write("Random Coin Value : " + result);
+
+// Question No 6
+// var RandomNumber = Math.floor(Math.random() * 100) + 1;
+// document.write("The Random Number Between 1 To 100 is: " + RandomNumber);
+
+// Question No 7
+
+// var WeightInput = prompt("Enter Your Weight In Kgs ");
+// var Weight = parseFloat(WeightInput); 
+
+// if (Weight) {
+//     document.write("Your weight is: " + Weight + " kgs");
+// } else {
+//     document.write("Please Enter a Valid Weight.");
+// }
+
+// Question No 8
+
+// var SecretNumber = Math.floor(Math.random() * 10) + 1;
+// var UserGuess = prompt("Guess a Number Between 1 To 10:");
+// UserGuess = +(UserGuess);
+
+// if (UserGuess === SecretNumber) {
+//     document.write("Congratulations! You Guessed The Right Number: " + SecretNumber);
+// } else {
+//     document.write("Sorry, The Secret Number Was: " + SecretNumber);
+// }
